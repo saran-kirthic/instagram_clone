19ce9c4a4a5a00a8b576e4baf77567bf838d02cc
saran <saranabcd465@gmail.com>
2023-03-15 18:12:54 +0530

    authentication

--- server/app.js
+++ server/app.js
@@ -6,9 +6,14 @@ const {MONGOURI} =require('./keys')
 
 require('./models/user')
 
+app.use(express.json())
 app.use(require('./routes/auth'))
 
-mongoose.connect(MONGOURI)
+
+mongoose.connect(MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
 mongoose.connection.on('connected',()=>{
     console.log('connected mongo yeahhhhh')
 })
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,4 +5,16 @@ router.get('/',(req,res)=>{
     res.send("yov bro")
 })
 
+router.get('/signup',(req,res)=>{
+    res.send("hi")
+})
+
+router.post('/signup',(req,res)=>{
+    const {name,email,password} = req.body
+    if(!email||!password||!name){
+        res.status(422).json({error:"please add all feilds"})
+    }
+    res.json({message:"succesfully posted"})
+})
+
 module.exports=router
