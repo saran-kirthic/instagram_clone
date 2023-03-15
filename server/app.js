19c10a9a11c063d2164e871984888bc198a49e7d
saran <saranabcd465@gmail.com>
2023-03-15 16:14:45 +0530

    test

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app= express()
+const mongoose = require('mongoose')
+const PORT = 5000
+const {MONGOURI} =require('./keys')
+
+require('./models/user')
+
+app.use(require('./routes/auth'))
+
+mongoose.connect(MONGOURI)
+mongoose.connection.on('connected',()=>{
+    console.log('connected mongo yeahhhhh')
+})
+mongoose.connection.on('error',()=>{
+    console.log('err connecting',err)
+})
+// mongo_atlas_database_password: 77aa856208qQzlJq
+
+
+app.get('/',(req,res)=>{
+    res.send("hello world")
+})
+
+
+app.listen(PORT,()=>{
+    console.log("server is running on",PORT)
+})
---
+++ server/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    MONGOURI:"mongodb+srv://saran:77aa856208qQzlJq@cluster0.vuhrp1n.mongodb.net/?retryWrites=true&w=majority"
+}
