6e3f39157326a4655e70ab89e71b790bb2af52d7
saran <saranabcd465@gmail.com>
2023-04-20 19:01:38 +0530

    likes and comments

--- client/src/components/screens/home.js
+++ client/src/components/screens/home.js
@@ -1,7 +1,10 @@
-import React,{useState,useEffect} from 'react';
+import React,{useState,useEffect, useContext} from 'react';
+
+import { UserContext } from '../../App';
 
 const Home = ()=>{
     const [data,setData]= useState([])
+    const {state,dispatch}=useContext(UserContext)
     useEffect(()=>{
         fetch('/allpost',{
             headers:{
@@ -12,6 +15,76 @@ const Home = ()=>{
             setData(result.posts)
         })
     },[])
+
+    const likePost=(id)=>{
+        fetch('/likes',{
+            method: "put",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId:id
+            })
+        }).then(res=>res.json())
+        .then(result=>{
+            const newData=data.map(item=>{
+                if(item._id==result._id){
+                    return result
+                }else{
+                    return item
+                }
+            })
+            setData(newData)
+        })
+    }
+
+    const unlikePost=(id)=>{
+        fetch('/unlikes',{
+            method: "put",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId:id
+            })
+        }).then(res=>res.json())
+        .then(result=>{
+            const newData=data.map(item=>{
+                if(item._id==result._id){
+                    return result
+                }else{
+                    return item
+                }
+            })
+            setData(newData)
+        })
+    }
+
+    const makeComment=(text,postId)=>{
+        fetch('/comments',{
+            method: "put",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId,
+                text
+            })
+        }).then(res=>res.json())
+        .then(result=>{
+            const newData=data.map(item=>{
+                if(item._id==result._id){
+                    return result
+                }else{
+                    return item
+                }
+            })
+            setData(newData)
+        })
+    }
 
     return(
         <div className='home'>
@@ -24,10 +97,23 @@ const Home = ()=>{
                                 <img src={item.photo}/>
                                 </div>
                 <div className='card-content'>
-                <i className="material-icons" style={{color:"red"}}>favorite</i>
-                    <h6>{item.title}</h6>
+                 {item.likes.includes(state._id)?<i className="material-icons" style={{color:"red"}} onClick={()=>{unlikePost(item._id)}}>favorite</i>:<i className="material-icons" style={{color:"red"}} onClick={()=>{likePost(item._id)}}>favorite_border</i>}                           
+                   <h6>{item.likes.length} likes</h6>
+                    <h6><b>{item.title}</b></h6>
                     <p>{item.body}</p>
-                    <input type='text' placeholder='add a comment' />
+                    {
+                        item.comments.map(record=>{
+                            return(
+                                <h6 key={record._id}><span style={{fontWeight:"500"}}><b>{record.postedBy.name}</b></span> {record.text}</h6>
+                            )
+                        })
+                    }
+                    <form onSubmit={(e)=>{
+                        e.preventDefault()
+                        makeComment(e.target[0].value,item._id)
+                    }}>
+                        <input type='text' placeholder='add a comment' />
+                    </form>
 
                 </div>
             </div>
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,6 +7,7 @@ const Post= mongoose.model("Post")
 router.get('/allpost',requireLogin,(req,res)=>{
     Post.find()
     .populate('postedBy','_id name')
+    .populate("comments.postedBy","_id name")
     .then(posts=>{
         res.json({posts})
     })
@@ -43,6 +44,54 @@ router.get('/mypost',requireLogin,(req,res)=>{
     })
     .catch(err=>{
         console.log(err)
+    })
+})
+
+router.put('/likes',requireLogin,(req,res)=>{
+    Post.findByIdAndUpdate(req.body.postId,{
+        $push:{likes:req.user._id}
+    },{
+        new:true
+    }).exec((err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+router.put('/unlikes',requireLogin,(req,res)=>{
+    Post.findByIdAndUpdate(req.body.postId,{
+        $pull:{likes:req.user._id}
+    },{
+        new:true
+    }).exec((err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+router.put('/comments',requireLogin,(req,res)=>{
+    const comment = {
+        text:req.body.text,
+        postedBy:req.user._id
+    }
+    Post.findByIdAndUpdate(req.body.postId,{
+        $push:{comments:comment}
+    },{
+        new:true
+    })
+    .populate("comments.postedBy","_id name")
+    .exec((err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }else{
+            res.json(result)
+        }
     })
 })
 
